543c01a6abd7527a3a9b73b2a61748313739b405
Garrick Aden-Buie <garrick@adenbuie.com>
2025-09-04 17:10:52 -0400

    chore: move icons to tool result

--- pkg-py/src/querychat/tools.py
+++ pkg-py/src/querychat/tools.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Callable
 
 from chatlas import ContentToolResult, Tool
+from htmltools import HTML
 from shinychat.types import ToolResultDisplay
 
 from ._utils import df_to_html
@@ -42,12 +43,7 @@ def tool_update_dashboard(
 
     """
 
-    @_as_tool(
-        annotations={
-            "title": "Update Dashboard",
-            "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/></svg>',
-        },
-    )
+    @_as_tool(annotations={"title": "Update Dashboard"})
     def update_dashboard(query: str, title: str) -> ContentToolResult:
         """
         Modify the data presented in the data dashboard, based on the given SQL query,
@@ -97,6 +93,9 @@ def tool_update_dashboard(
                     title=title,
                     show_request=False,
                     open=True,
+                    icon=HTML(
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/></svg>',
+                    ),
                 ),
             },
         )
@@ -125,12 +124,7 @@ def tool_reset_dashboard(
 
     """
 
-    @_as_tool(
-        annotations={
-            "title": "Reset Dashboard",
-            "icon": '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="bi bi-arrow-counterclockwise" style="height:1em;width:1em;fill:currentColor;vertical-align:-0.125em;" aria-hidden="true" role="img"><path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"></path><path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"></path></svg>',
-        },
-    )
+    @_as_tool(annotations={"title": "Reset Dashboard"})
     def reset_dashboard() -> ContentToolResult:
         """
         Reset the data dashboard to show all data.
@@ -156,6 +150,9 @@ def tool_reset_dashboard(
                     title=None,
                     show_request=False,
                     open=False,
+                    icon=HTML(
+                        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="bi bi-arrow-counterclockwise" style="height:1em;width:1em;fill:currentColor;vertical-align:-0.125em;" aria-hidden="true" role="img"><path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"></path><path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"></path></svg>',
+                    ),
                 ),
             },
         )
@@ -179,12 +176,7 @@ def tool_query(data_source: DataSource) -> Tool:
 
     """
 
-    @_as_tool(
-        annotations={
-            "title": "Query Data",
-            "icon": '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/></svg>',
-        },
-    )
+    @_as_tool(annotations={"title": "Query Data"})
     # TODO: Replace title with `_intent` if supported directly by chatlas
     def query(query: str, title: str = "") -> ContentToolResult:
         """
@@ -224,11 +216,11 @@ def tool_query(data_source: DataSource) -> Tool:
                     title=f"Query: {title}" if title else None,
                     show_request=False,
                     open=True,
+                    icon=HTML(
+                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/></svg>',
+                    ),
                 ),
             },
         )
 
     return query
-
-
-# The querychat_tool_result function is no longer needed since we use ContentToolResult directly

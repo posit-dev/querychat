4291e4b92f544c9df243c0203915475177ad1f12
Carson <cpsievert1@gmail.com>
2025-11-12 13:16:25 -0600

    Fix format check

--- pkg-py/src/querychat/__init__.py
+++ pkg-py/src/querychat/__init__.py
@@ -1,7 +1,6 @@
 from querychat._greeting import greeting
-from querychat.querychat import QueryChat, init
+from querychat.querychat import QueryChat, init, sidebar, system_prompt
 from querychat.querychat import mod_server as server
 from querychat.querychat import mod_ui as ui
-from querychat.querychat import sidebar, system_prompt
 
 __all__ = ["QueryChat", "greeting", "init", "server", "sidebar", "system_prompt", "ui"]

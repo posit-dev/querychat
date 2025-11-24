d3479cb62a6bec354822c360151cc2a308e25ffe
Carson Sievert <cpsievert1@gmail.com>
2025-11-24 09:59:57 -0600

    feat(pkg-py): Add `.app()` method, enable bookmarking by default (#104)
    * feat(pkg-py): Add .app() method, enable bookmarking by default
    
    * fix: icon bug
    
    * Address feedback
    
    * Default enable_bookmarking to False; auto-enable in Express if a store is set
    
    * Revert shiny version requirement

--- pkg-py/src/querychat/tools.py
+++ pkg-py/src/querychat/tools.py
@@ -5,9 +5,9 @@ from typing import TYPE_CHECKING, Callable
 
 import chevron
 from chatlas import ContentToolResult, Tool
-from htmltools import HTML
 from shinychat.types import ToolResultDisplay
 
+from ._icons import bs_icon
 from ._utils import df_to_html
 
 if TYPE_CHECKING:
@@ -66,9 +66,7 @@ def _update_dashboard_impl(
                     title=title,
                     show_request=False,
                     open=True,
-                    icon=HTML(
-                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel-fill" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5z"/></svg>',
-                    ),
+                    icon=bs_icon("funnel-fill"),
                 ),
             },
         )
@@ -142,9 +140,7 @@ def _reset_dashboard_impl(
                     title=None,
                     show_request=False,
                     open=False,
-                    icon=HTML(
-                        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="bi bi-arrow-counterclockwise" style="height:1em;width:1em;fill:currentColor;vertical-align:-0.125em;" aria-hidden="true" role="img"><path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"></path><path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"></path></svg>',
-                    ),
+                    icon=bs_icon("arrow-counterclockwise"),
                 ),
             },
         )
@@ -213,9 +209,7 @@ def _query_impl(data_source: DataSource) -> Callable[[str, str], ContentToolResu
                     markdown=markdown,
                     show_request=False,
                     open=True,
-                    icon=HTML(
-                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16"><path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/></svg>',
-                    ),
+                    icon=bs_icon("table"),
                 ),
             },
         )

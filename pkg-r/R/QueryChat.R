3e030fdc9a84b3c687b25af16fb85fba0d8aec4e
Garrick Aden-Buie <garrick@adenbuie.com>
2026-06-22 10:32:57 -0400

    fix: exclude control inputs from Shiny bookmarking
    Inputs owned by querychat -- chat_update, close_btn, reset_query, and
    the ephemeral viz code editor -- are excluded from Shiny's bookmark
    state. Inputs owned by shinychat (chat_user_input, chat_cancel, etc.)
    are handled upstream by posit-dev/shinychat#259.

--- pkg-r/R/QueryChat.R
+++ pkg-r/R/QueryChat.R
@@ -531,6 +531,7 @@ QueryChat <- R6::R6Class(
       }
 
       server <- function(input, output, session) {
+        shiny::setBookmarkExclude(c("close_btn", "reset_query"))
         # Enable bookmarking if bookmark_store is enabled
         enable_bookmarking <- bookmark_store %in% c("url", "server")
         qc_vals <- self$server(enable_bookmarking = enable_bookmarking)

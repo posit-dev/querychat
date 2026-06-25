20b5b4570b4c727968478c4f3dad69986cee970c
Garrick Aden-Buie <garrick@adenbuie.com>
2026-06-25 16:19:04 -0400

    chore: add TODO for chat_greeting_persistent removal

--- pkg-r/R/utils-shinychat.R
+++ pkg-r/R/utils-shinychat.R
@@ -1,5 +1,6 @@
 # shinychat compatibility helpers
 
+# TODO: remove once shinychat >= 0.5.0 is the minimum (persistent added in 0.4.0.9000)
 chat_greeting_persistent <- function(content) {
   if (utils::packageVersion("shinychat") > "0.4.0") {
     shinychat::chat_greeting(content, persistent = TRUE)
